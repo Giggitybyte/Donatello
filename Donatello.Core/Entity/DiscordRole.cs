52155edf23422d82429f0b71c71a8f6a9bfb80e7
Luke Triano <luketriano115@gmail.com>
2022-05-02 17:53:48 -0700

    Entity reset

--- Donatello.Core/Entity/DiscordRole.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Donatello.Entity;
-
-using System.Text.Json;
-
-public sealed class DiscordRole : DiscordEntity
-{
-    public DiscordRole(DiscordApiBot bot, JsonElement jsonObject) : base(bot, jsonObject) { }
-}
